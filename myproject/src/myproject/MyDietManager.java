1d65656615b906f7888945a79f899a706bf41f09
Your Name <you@example.com>
2017-09-28 12:55:14 +0900

    read file by internet

--- myproject/src/myproject/MyDietManager.java
+++ myproject/src/myproject/MyDietManager.java
@@ -7,6 +7,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -107,6 +108,19 @@ public class MyDietManager {
 		        }catch(Exception e) {
 		        	
 		        }
+			}else if(type == 5) {
+				try {
+					URL oracle = new URL("http://keenset.dothome.co.kr/MyData.csv");
+				    BufferedReader in = new BufferedReader(
+				    new InputStreamReader(oracle.openStream()));
+	
+				    String inputLine;
+				    while ((inputLine = in.readLine()) != null)
+				        System.out.println(inputLine);
+				    in.close();
+				}catch(Exception e) {
+					
+				}
 			}
 		}
 	}
